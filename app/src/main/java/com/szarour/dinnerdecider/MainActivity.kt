6acd656cbe07b67f9f9f74324dad3fee623a3ccc
zaroursamy <samy.zarour8430@gmail.com>
2019-11-22 11:01:17 +0100

    Add remove food button

--- app/src/main/java/com/szarour/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/szarour/dinnerdecider/MainActivity.kt
@@ -8,29 +8,50 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
 
 
-
-    val foodList = arrayListOf("Chinese", "Mexican", "Burger", "Pizza", "Pasta", "Fish & Chips", "Salad", "Vegan Burger", "Kebab")
+    val foodList = arrayListOf(
+        "Chinese",
+        "Mexican",
+        "Burger",
+        "Pizza",
+        "Pasta",
+        "Fish & Chips",
+        "Salad",
+        "Vegan Burger",
+        "Kebab"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         decideBtn.setOnClickListener {
-            var random = Random().nextInt(foodList.size)
-            val currentIndex = foodList.indexOf(selectedFoodTxt.text)
-            while(random == currentIndex){
-                random = Random().nextInt(foodList.size)
+            if (foodList.isNotEmpty()) {
+                var random = Random().nextInt(foodList.size)
+                val currentIndex = foodList.indexOf(selectedFoodTxt.text)
+                while (random == currentIndex) {
+                    random = Random().nextInt(foodList.size)
+                }
+                selectedFoodTxt.text = foodList[random]
             }
-            selectedFoodTxt.text = foodList[random]
         }
 
         addFoodBtn.setOnClickListener {
             val txt = addFoodTxt.text.toString()
-            if(txt.isNotEmpty()) foodList.add(txt) else println("Empty text to add")
-            addFoodTxt.text.clear()
-            selectedFoodTxt.text = txt
+            if (txt.isNotEmpty()) {
+                foodList.add(txt)
+                addFoodTxt.text.clear()
+                selectedFoodTxt.text = txt
+            } else println("Empty text to add")
+
         }
 
-
+        removeBtn.setOnClickListener {
+            val txt = selectedFoodTxt.text.toString()
+            foodList.remove(txt)
+            if (foodList.isNotEmpty()) {
+                val random = Random().nextInt(foodList.size)
+                selectedFoodTxt.text = foodList[random]
+            } else selectedFoodTxt.text = "vide"
+        }
     }
 }
